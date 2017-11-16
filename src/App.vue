a4f1cd35986b5ca5d6d1cd05a4547296e54feef0
mohamed mhiri <mohamed.mohamed.mhiri@gmail.com>
2017-11-16 03:44:27 +0100

    material-design-lite added

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <span>Vue.js PWA</span>
     </header>
     <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
+      <img src="./assets/logo.png" alt="FIX PWA">
       <router-view></router-view>
     </main>
   </div>
@@ -15,40 +15,8 @@ export default {
   name: 'app'
 }
 </script>
-
+  require('material-design-lite')
 <style>
-body {
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
+  @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+  @import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');
 </style>
