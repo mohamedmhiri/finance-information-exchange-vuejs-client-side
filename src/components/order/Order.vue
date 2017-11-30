92e687f4fc89829997da9b0e07ce29bb78f8d3fa
mohamed mhiri <mohamed.mohamed.mhiri@gmail.com>
2017-11-30 09:52:05 +0100

    materialize-css deleted

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -1,13 +1,53 @@
 <template>
-  <p>Passer un ordre</p>
+    <form class="mui-form">
+      <legend>Passer un ordre</legend>
+      <div class="mui-select">
+        <select>
+          <option>Achat</option>
+          <option>Vente</option>
+        </select>
+        <label>Sens</label>
+      </div>
+      <div class="mui-select">
+        <select>
+          <option v-for="item in market" v-text="item.name"></option>
+        </select>
+        <label>Sens</label>
+      </div>
+      <div class="mui-textfield mui-textfield--float-label">
+        <input type="text">
+        <label>Input 1</label>
+      </div>
+      <div class="mui-textfield mui-textfield--float-label">
+        <input type="text">
+        <label>Input 2</label>
+      </div>
+      <div class="mui-textfield mui-textfield--float-label">
+        <textarea></textarea>
+        <label>Textarea</label>
+      </div>
+      <button type="submit" class="mui-btn mui-btn--raised">Ajouter</button>
+    </form>
+
 </template>
 
 <script>
+  import axios from 'axios'
   export default {
     name: 'order',
     data () {
       return {
+        market: []
       }
+    },
+    mounted () {
+      axios.get(`http://localhost:3000/api/market`)
+        .then(response => {
+          this.market = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
   }
 </script>
